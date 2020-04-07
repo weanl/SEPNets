136db53204e3cfadf345602831b3a99173d63165
weanl <1975346264@qq.com>
2020-04-07 11:14:37 +0800

    configure other dataset from envy

--- utils/tools.py
+++ utils/tools.py
@@ -1,4 +1,14 @@
 
+import sys
+import os
+from datetime import datetime
+
+curPath = os.path.abspath(os.path.dirname(__file__))
+rootPath = os.path.split(curPath)[0]
+sys.path.append(rootPath)
+
+
+from utils.configs import get_config_from_json
 from scipy.signal import correlate
 import numpy as np
 import statsmodels.api as sm
@@ -7,6 +17,44 @@ from scipy import stats
 from statsmodels.graphics.api import qqplot
 
 from sklearn.metrics import mean_absolute_error
+import json
+
+
+EXP_DIRS = ['../../exp_ElectricityLoad/', 
+            '../../exp_210100063/',
+            '../../exp_201812/',
+            '../../exp_210100112/']
+EXP_DIR = EXP_DIRS[3]
+exp_config, _exp_config = get_config_from_json(EXP_DIR + 'exp_config.json')
+N_VAR = exp_config.N_VAR
+VARS = exp_config.VARS
+Max_Window = exp_config.Max_Window
+Max_Epoch = exp_config.Max_Epoch
+
+
+def create_json_file(data, file):
+    with open(file, 'w') as f:
+        json.dump(data, f)
+    return
+
+
+def create_json_files(models=[]):
+    commom_dict = {
+        "optimizer": "Adam", 
+        "lag": Max_Window, 
+        "batch_size": 32, 
+        "epochs": Max_Epoch, 
+        "option_lags": [Max_Window]
+    }
+    for model in models:
+        [
+            create_json_file(
+                data=commom_dict,
+                file=EXP_DIR+'saved_models/'+model+'/'+model+'_'+v+'_configs.json'
+            )
+            for v in VARS
+        ]
+    return
 
 
 def slide_window_x(x, look_back=60, forecast_step=10, test_flag=False):
@@ -84,4 +132,5 @@ if __name__ == '__main__':
     # test_mae()
     # res = mean_mae(np.array([[1,1], [2, 2], [2, 2], [3, 3]]))
     # print(res, res.mean())
+    create_json_files(['ar', 'are', 'ear'])
     pass
